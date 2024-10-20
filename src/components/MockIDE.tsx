585ababaad2e4ac0489c9f8a384cdb7e68d39436
SundarRajendiran <155504903+SundarRajendiran@users.noreply.github.com>
2024-10-20 11:59:36 +0530

    Inline style removed

--- src/components/MockIDE.tsx
+++ src/components/MockIDE.tsx
@@ -86,7 +86,7 @@ export function MockIDE() {
         <ReflexElement minSize={200} flex={0.25}>
           <div className="p-4">
             {isLoadingFiles ? (
-              <Skeleton count={5} height={20} style={{ marginBottom: '10px' }} />
+              <Skeleton count={5} height={20}/>
             ) : (
               <TreeView
                 files={fileSystem.files}
